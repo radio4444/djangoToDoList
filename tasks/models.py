9ffb7aa831767146762caf0c68ca86b16b088e7b
Tanzil[laptop] <46586118+radio4444@users.noreply.github.com>
2023-04-09 13:13:58 -0400

    Add class tasks and str.

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 from django.utils import timezone
+
+
 # Create your models here.
+class Task(models.Model):
+    Priority_choices = [  # nested List, where it contains tuples
+        ('L', 'Low'),
+        ('M', 'Medium'),
+        ('H', 'High')
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()  # for large amounts of text
+    priority = models.CharField(max_length=1, choices=Priority_choices, defaults='M')
+    due_date = models.DateTimeField(default=timezone.now)
+    completed = models.BooleanField(default=False)
+
+
+# How tasks should be displayed in Django Admin interface
+
+def _str_(self):
+    return self.title
